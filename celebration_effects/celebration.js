1c16738c3ff3bb82c6b761f995da6683c6898cb0
Raven <157051380+raven-elizabeth@users.noreply.github.com>
2025-03-20 22:50:33 +0000

    Added work in progress multi-festive project

---
+++ celebration_effects/celebration.js
@@ -0,0 +1,15 @@
+const celebrations = document.getElementsByClassName("celebrations")[0];
+
+// Unfinished for now, will add button click functionality
+// for (let i = 0 < celebrations.length; i++) {
+// const holiday = document.getElementsByClassName("holiday")[0];
+// holiday.addEventListener("click", function () {
+// holiday
+// }
+// }
+
+// function halloween
+
+// function disclaimer(note) {
+
+//} 
